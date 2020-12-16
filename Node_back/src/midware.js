c1c939328d1dfb523cd663cd0cc73e5955bd0e55
Ramunas <zamrif@gmail.com>
2020-12-16 08:31:19 +0200

    comments added

--- Node_back/src/midware.js
+++ Node_back/src/midware.js
@@ -1,15 +1,12 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+// validating the user input data by length
 module.exports = {
   validateUserData: (req, res, next) => {
     const register = req.body;
 
-    if (register.username === register.password) {
-      res.status(400).json({ msg: "username can't be same as password" });
-    }
-
-    if (!register.username || register.username.length < 2) {
+    if (!register.email || register.email.length < 2) {
       res.status(400).json({ msg: "username is too short" });
     }
 
@@ -20,10 +17,14 @@ module.exports = {
     next();
   },
 
+  //  validating if the user is logged in correclty (with token)
   LoggedIn: (req, res, next) => {
     try {
+      // getting token from request header
       const token = req.headers.authorization.split(" ")[1];
+      // vertifying recievde token by back-end secret key
       const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+      // token is used to identify the correct session user so it is passed forward as userData
       req.userData = decodedToken;
       next();
     } catch (err) {
--- Node_back/src/router.js
+++ Node_back/src/router.js
@@ -19,25 +19,30 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", midware.validateUserData, (req, res) => {
-  const username = req.body.username.toLowerCase();
+  // const are precoded not to cloister rest of the code
+  const email = req.body.email;
   const user = req.body;
+
+  // checking the avalability of unique user to register
   con.query(
-    `SELECT * FROM users WHERE username = ${mysql.escape(username)}`,
+    `SELECT * FROM users WHERE email = ${mysql.escape(email)}`,
     (err, result) => {
       if (err) return res.status(400).json({ msg: err });
       else if (result != 0) {
-        res.status(400).json({ msg: "user already exist" });
+        res.status(400).json({ msg: "user already created under this email" });
+        // encrypting the users password and created hash will be stored into DB (not original password)
       } else {
         bcrypt.hash(user.password, 10, (err, hash) => {
           if (err) return res.status(400).json({ msg: err });
           con.query(
-            `INSERT INTO users (username, password) VALUES (${mysql.escape(
-              username
-            )}, ${mysql.escape(hash)})`,
+            // inserting values with mysql.escape function so that it can't be hack by input
+            `INSERT INTO users (email, password, team) VALUES (${mysql.escape(
+              email
+            )}, ${mysql.escape(hash)}, ${mysql.escape(user.team)})`,
             (err, result) => {
               if (err) return res.status(400).json({ msg: err });
               res.status(201).json({
-                msg: `${username} successfully registered`,
+                msg: `${user.team} successfully registered`,
               });
             }
           );
@@ -48,36 +53,43 @@ router.post("/register", midware.validateUserData, (req, res) => {
 });
 
 router.post("/login", midware.validateUserData, (req, res) => {
-  const username = req.body.username.toLowerCase();
+  const email = req.body.email;
   const loginPass = req.body.password;
 
-  if (username.length === 0) {
+  // validating input length
+  if (email.length === 0) {
     res.status(400).json({ msg: "please type in username" });
   } else if (loginPass.length === 0) {
     res.status(400).json({ msg: "please type in password" });
   } else {
     con.query(
-      `SELECT * FROM users WHERE username = ${mysql.escape(username)}`,
+      // validating by selecting data from specific DB collum
+      `SELECT * FROM users WHERE email = ${mysql.escape(email)}`,
       (err, result) => {
         if (err) return res.status(400).json({ msg: err });
+        // and  checking if the user exist in DB
         else if (result.length === 0) {
           res.status(400).json({ msg: "user not found in database" });
         } else {
+          // if user exist getting his password
           const storedPass = result[0].password;
-
+          // using bcrypt.compare function to decode and match passwords from input and DB
           bcrypt.compare(loginPass, storedPass, (compareErr, compareResult) => {
             if (compareErr || !compareResult)
               res
                 .status(400)
                 .json({ msg: "username or password doesn't match" });
             else {
+              // if the passwords match user token is created with jwt and secret key stored in environmental variables
               const token = jwt.sign(
                 { userID: result[0].id, user: username },
                 process.env.SECRET_KEY,
+                // adding expirasion date of 72 hours
                 {
                   expiresIn: Math.floor(Date.now() / 1000) + 60 * 60 * 72,
                 }
               );
+              // and sending created token back
               res.status(202).json({
                 msg: "logged in ",
                 token,
