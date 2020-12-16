f4834d18acaae54e62050682057d3fe45f5b402b
Ramunas <zamrif@gmail.com>
2020-12-16 18:45:53 +0200

    Navigation styled

--- React_front/src/components/Navigation/Navigation.jsx
+++ React_front/src/components/Navigation/Navigation.jsx
@@ -5,21 +5,33 @@ import PropType from "prop-types";
 function Navigation({ loggedIn, logout }) {
   return (
     <S.Header>
-      {loggedIn && (
-        <>
-          <S.StyledLink to="/player">PLAYER</S.StyledLink>
-          <S.StyledLink to="/team">TEAM</S.StyledLink>
-          <S.StyledLink onClick={logout} to="/login">
-            LOGOUT
-          </S.StyledLink>
-        </>
-      )}
-      {!loggedIn && (
-        <>
-          <S.StyledLink to="/login">LOGIN</S.StyledLink>
-          <S.StyledLink to="/register">REGISTER</S.StyledLink>
-        </>
-      )}
+      <S.Action>
+        {loggedIn && (
+          <>
+            <S.LinkBlock>
+              <S.StyledLink to="/player">PLAYER</S.StyledLink>
+            </S.LinkBlock>
+            <S.LinkBlock>
+              <S.StyledLink to="/team">TEAM</S.StyledLink>
+            </S.LinkBlock>
+            <S.LinkBlock>
+              <S.StyledLink onClick={logout} to="/login">
+                LOGOUT
+              </S.StyledLink>
+            </S.LinkBlock>
+          </>
+        )}
+        {!loggedIn && (
+          <>
+            <S.LinkBlock>
+              <S.StyledLink to="/login">LOGIN</S.StyledLink>
+            </S.LinkBlock>
+            <S.LinkBlock>
+              <S.StyledLink to="/register">REGISTER</S.StyledLink>
+            </S.LinkBlock>
+          </>
+        )}
+      </S.Action>
     </S.Header>
   );
 }
--- React_front/src/components/Navigation/Navigation.styled.js
+++ React_front/src/components/Navigation/Navigation.styled.js
@@ -2,28 +2,37 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1em 2em;
-  border-bottom: 2px solid #000;
+  position: absolute;
+  top: 0;
+  min-width: 100%;
 `;
 
-export const Action = styled.div``;
+export const Action = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const LinkBlock = styled.div`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 900;
+  font-size: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 1em;
+  height: calc(100vh - 2em);
+  width: 33%;
+  &:hover {
+    background-color: rgba(241, 90, 36, 0.6);
+    transition: 1s;
+  }
+`;
 
 export const StyledLink = styled(Link)`
-  color: #000;
-  text-decoration: none;
+  color: #fff;
   cursor: pointer;
-  &:hover {
-    text-decoration: underline;
-  }
+  text-decoration: none;
   &:not(:last-child) {
     padding-right: 2em;
   }
-`;
-
-export const Logo = styled.img`
-  max-height: 2em;
-  max-width: 100%;
 `;
