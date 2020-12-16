f821b7b1f620ca38e4aac7216d92d811b8d4c24f
Ramunas <zamrif@gmail.com>
2020-12-16 17:30:52 +0200

    navigation moved to pages

--- React_front/src/Routes.js
+++ React_front/src/Routes.js
@@ -1,7 +1,5 @@
-import React, { Suspense, lazy, useContext } from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navigation } from "./components";
-import { AuthContext } from "./contexts/AuthContext";
 
 const HomeLazy = lazy(() => import("./pages/Home/Home.jsx"));
 const PlayerLazy = lazy(() => import("./pages/Player/Player.jsx"));
@@ -10,11 +8,8 @@ const LoginLazy = lazy(() => import("./pages/Login/Login.jsx"));
 const RegisterLazy = lazy(() => import("./pages/Register/Register.jsx"));
 
 function Routes() {
-  const auth = useContext(AuthContext);
-
   return (
     <Router>
-      <Navigation loggedIn={!!auth.token} logout={() => auth.setToken("")} />
       <Switch>
         <Suspense fallback="loading">
           <Route exact path="/" component={HomeLazy} />
