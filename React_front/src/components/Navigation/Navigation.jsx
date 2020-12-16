c23de38fabcf921749ebaf462da3f20b6216fcc8
Ramunas <zamrif@gmail.com>
2020-12-16 17:34:22 +0200

    logo removed from navigation

--- React_front/src/components/Navigation/Navigation.jsx
+++ React_front/src/components/Navigation/Navigation.jsx
@@ -1,33 +1,25 @@
 import React from "react";
 import * as S from "./Navigation.styled";
-import { Link } from "react-router-dom";
-import LogoImg from "../../assets/logo.png";
 import PropType from "prop-types";
 
 function Navigation({ loggedIn, logout }) {
   return (
     <S.Header>
-      <Link to="/">
-        <S.Logo src={LogoImg} alt="coral logo" />
-      </Link>
-
-      <S.Action>
-        {loggedIn && (
-          <>
-            <S.StyledLink to="/player">PLAYER</S.StyledLink>
-            <S.StyledLink to="/team">TEAM</S.StyledLink>
-            <S.StyledLink onClick={logout} to="/login">
-              LOGOUT
-            </S.StyledLink>
-          </>
-        )}
-        {!loggedIn && (
-          <>
-            <S.StyledLink to="/login">LOGIN</S.StyledLink>
-            <S.StyledLink to="/register">REGISTER</S.StyledLink>
-          </>
-        )}
-      </S.Action>
+      {loggedIn && (
+        <>
+          <S.StyledLink to="/player">PLAYER</S.StyledLink>
+          <S.StyledLink to="/team">TEAM</S.StyledLink>
+          <S.StyledLink onClick={logout} to="/login">
+            LOGOUT
+          </S.StyledLink>
+        </>
+      )}
+      {!loggedIn && (
+        <>
+          <S.StyledLink to="/login">LOGIN</S.StyledLink>
+          <S.StyledLink to="/register">REGISTER</S.StyledLink>
+        </>
+      )}
     </S.Header>
   );
 }
