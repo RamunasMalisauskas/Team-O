c097e3959710aeeadae8b92df2930dab4b714c0a
Ramunas <zamrif@gmail.com>
2020-12-17 19:19:38 +0200

    sticky postion props added to button

--- React_front/src/components/Button/Button.jsx
+++ React_front/src/components/Button/Button.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import * as S from "./Button.styled";
 import PropType from "prop-types";
 
-function Button({ children, handleClick, color, size, buttonType }) {
+function Button({ children, handleClick, color, sticky, size, buttonType }) {
   return (
-    <S.Button onClick={handleClick} color={color} size={size} type={buttonType}>
+    <S.Button
+      onClick={handleClick}
+      color={color}
+      sticky={sticky}
+      size={size}
+      type={buttonType}
+    >
       {children}
     </S.Button>
   );
@@ -15,6 +21,7 @@ Button.propTypes = {
   color: PropType.string,
   size: PropType.string,
   buttonType: PropType.string,
+  sticky: PropType.bool,
 };
 
 export default Button;
