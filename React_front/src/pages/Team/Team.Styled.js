0e9373a70f84f8187516ec8e9b06b9ffd663c500
Ramunas <zamrif@gmail.com>
2020-12-18 20:58:56 +0200

    page Team created and styled

---
+++ React_front/src/pages/Team/Team.Styled.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import backImg from "../../assets/playerImg.jpg";
+
+export const PageBackground = styled.div`
+  position: fixed;
+  z-index: -1;
+  background: url(${backImg}) center top;
+  object-fit: cover;
+  height: ${(props) => props.theme.fullHeight};
+  width: 100%;
+
+  @media (max-width: ${(props) => props.theme.websiteWidth}) {
+    background: url(${backImg});
+  }
+`;
+
+export const Frame = styled.div`
+  border: ${(props) => props.theme.primary.border};
+  padding: ${(props) => props.theme.standart.gutterSize};
+  background-color: ${(props) => props.theme.secondary.color};
+
+  @media (max-width: ${(props) => props.theme.websiteWidth}) {
+    padding: ${(props) => props.theme.tiny.gutterSize};
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: right;
+  font-family: ${(props) => props.theme.primary.font};
+  font-weight: ${(props) => props.theme.primary.fontSize};
+`;
+
+export const Block = styled.div`
+  width: 50vw;
+  margin-top: ${(props) => props.theme.standart.gutterSize};
+  padding: ${(props) => props.theme.large.gutterSize};
+  background: ${(props) => props.theme.primary.background};
+`;
+
+export const Logo = styled.img`
+  height: ${(props) => props.theme.standart.height};
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+
+  @media (max-width: ${(props) => props.theme.websiteWidth}) {
+    height: ${(props) => props.theme.standart.height};
+  }
+`;
--- React_front/src/pages/Team/Team.jsx
+++ React_front/src/pages/Team/Team.jsx
@@ -1,11 +1,44 @@
-import React, { useContext } from "react";
-import { Section } from "../../components";
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Section, Button, Input, Notification } from "../../components";
 import { AuthContext } from "../../contexts/AuthContext";
+import * as S from "./Team.Styled";
+import logoImg from "../../assets/logo.svg";
 
 function Team() {
   const auth = useContext(AuthContext);
+  const [data, setData] = useState({});
+  const [error, setError] = useState({ status: false, msg: "", color: "" });
 
-  return <Section> {auth.token ? "This is secret page you have unlocked" : "you forgot to login"}</Section>;
+  return (
+    <>
+      <S.PageBackground />
+
+      <Section
+        center={true}
+        background={(props) => props.theme.support.background}
+      >
+        {/* Notification is turn on and off by "error" object status property */}
+        {error.status && (
+          <Notification
+            notificationMessage={error.msg}
+            handleClick={() => setError({ status: false })}
+            color={error.color}
+          />
+        )}
+
+        <S.Block>
+          <S.Title>TEAMS</S.Title>
+
+          <S.Frame></S.Frame>
+        </S.Block>
+      </Section>
+
+      <Link to="/">
+        <S.Logo src={logoImg} alt="teamo logo" />
+      </Link>
+    </>
+  );
 }
 
 export default Team;
