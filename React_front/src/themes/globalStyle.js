486377df999a5ca016621c8c906a9a84bd337c5f
Ramunas <zamrif@gmail.com>
2020-12-21 16:32:37 +0200

    global style update

--- React_front/src/themes/globalStyle.js
+++ React_front/src/themes/globalStyle.js
@@ -8,21 +8,6 @@ const globalStyle = createGlobalStyle`
     font-family: "Montserrat, sans-serif";
   }
 
-  p {
-    font-family: "Montserrat, sans-serif";
-    font-weight: 500
-  }
-
-  h2 {
-    text-align: right;
-    font-family: "Montserrat, sans-serif";
-    font-weight: 800
-  }
-
-  h3 {
-    font-family: "Montserrat, sans-serif";
-    font-weight: 800
-  }
 `;
 
 export default globalStyle;
